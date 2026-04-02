7067f9640f221444bc5e8244536f330d9312637e
“Chirag-skillcrafters” <chirag.skillcrafters@gmail.com>
2026-04-02 16:56:28 +0530

    responsive Client page

--- client/src/layout/DashboardLayout.jsx
+++ client/src/layout/DashboardLayout.jsx
@@ -9,10 +9,12 @@ const DashboardLayout = ({ children }) => {
     <div className='flex bg-slate-950 text-white min-h-screen'>
       <Sidebar setMenuBtn={setMenuBtn} menuBtn={menuBtn} />
 
-      <div className='flex-1 flex flex-col'>
+      <div className='flex-1 flex flex-col min-w-0 overflow-x-hidden'>
         <Navbar setMenuBtn={setMenuBtn} menuBtn={menuBtn} />
 
-        <main className='p-6 w-full flex-1'>{children}</main>
+        <main className='p-6 w-full flex-1 overflow-x-hidden min-w-0'>
+          {children}
+        </main>
       </div>
     </div>
   );

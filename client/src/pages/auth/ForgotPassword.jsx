db6c566927cadf8ff373d4b188180a487da183fb
“Chirag-skillcrafters” <chirag.skillcrafters@gmail.com>
2026-04-02 18:55:44 +0530

    change forget-password URL

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -10,7 +10,8 @@ function ForgotPassword() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/email/password-reset",
+        "http://localhost:5000/api/email/password-reset" ||
+          `${import.meta.env.VITE_API_URL}/api/email/password-reset`,
         { email },
       );
 
@@ -26,30 +27,29 @@ function ForgotPassword() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 dark:bg-gray-900">
+    <div className='min-h-screen flex items-center justify-center bg-slate-900 dark:bg-gray-900'>
       {successMessage && (
-        <div className="fixed top-4 right-4 bg-green-500 text-white p-4 rounded">
+        <div className='fixed top-4 right-4 bg-green-500 text-white p-4 rounded'>
           {successMessage}
         </div>
       )}
       <form
         onSubmit={handleSubmit}
-        className="bg-slate-800/80 backdrop-blur-2xl dark:bg-gray-800 p-6 rounded-lg w-80"
-      >
-        <h2 className="text-xl mb-4  dark:text-white text-gray-200">
+        className='bg-slate-800/80 backdrop-blur-2xl dark:bg-gray-800 p-6 rounded-lg w-80'>
+        <h2 className='text-xl mb-4  dark:text-white text-gray-200'>
           Forgot Password
         </h2>
 
         <input
-          type="email"
-          placeholder="Enter your email"
-          className="border border-gray-600 bg-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 ring-1 ring-blue-900 focus:ring-blue-500 w-full my-3 text-white"
+          type='email'
+          placeholder='Enter your email'
+          className='border border-gray-600 bg-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 ring-1 ring-blue-900 focus:ring-blue-500 w-full my-3 text-white'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
 
-        <button className="w-full bg-blue-500 text-white p-2 rounded">
+        <button className='w-full bg-blue-500 text-white p-2 rounded'>
           Send Reset Link
         </button>
       </form>

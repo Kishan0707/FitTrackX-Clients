e3f7eb958dec7457089a38c415b210abf11ccf8f
“Chirag-skillcrafters” <chirag.skillcrafters@gmail.com>
2026-04-03 10:24:02 +0530

    .env

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -7,8 +7,7 @@ function ForgotPassword() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const BASE_URL =
-      import.meta.env.VITE_API_BACKEND_URL || "http://localhost:5000";
+    const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
     try {
       const res = await axios.post(`${BASE_URL}/api/email/password-reset`, {
